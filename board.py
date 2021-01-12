2ce51a158fd6b088f3a5bd4f5a18eb662d8941b5
snord883 <steven.nord88@gmail.com>
2021-01-12 18:37:31 -0500

    Completed merge conflicts.

--- board.py
+++ board.py
@@ -9,6 +9,10 @@ class Board():
     master = None
     board = None
     player_up = 0
+    side_i = None
+    hole_i = None
+    currently_moving = False
+    marbles_in_hand = 0
     scoreboard = None
     game_board = None
    
@@ -24,26 +28,25 @@ class Board():
         _ = [self.scoreboard[i].reset() for i in range(2)]
         _ = [[self.game_board[j][i].reset() for i in range(6)] for j in range(2)]
 
-    def move_marbles(self,side_i,hole_i,n_marbles):
-        if n_marbles==0:
-            if hole_i == 6:
-                print("EXTRA TURN!!")
-                return None
-            elif not self.game_board[side_i][hole_i].get_marbles():
-                if side_i==self.player_up:
-                    self.scoreboard[self.player_up].add_marbles(self.game_board[int(not self.player_up)][5-hole_i])
-
-        hole_i += 1
-        if hole_i == 6:
-            side_i, hole_i, n_marbles = self.reached_store(side_i,n_marbles)
+    def move_marbles(self):
+        if self.marbles_in_hand==1:
+            self.handle_last_marble()
         else:
-            self.game_board[side_i][hole_i].add_marble()
-            n_marbles -= 1
-        self.master.after(1500, self.move_marbles,side_i,hole_i,n_marbles)
+            self.drop_marble_and_continue()
+        
+        if self.currently_moving:
+            self.master.after(speed_of_play, self.move_marbles)
+        else:
+            return None
 
-    def reached_store(self,side_i,n_marbles):
-        if side_i == self.player_up:
-            print('score one for PLAYER {}: {} marbles remain in hand'.format(side_i,n_marbles-1))
+    def handle_last_marble(self):
+        if self.is_this_a_store():
+            self.handle_last_marble_for_store()
+        else:
+            self.handle_last_marble_for_hole()
+
+    def handle_last_marble_for_store(self):
+        if self.side_i==self.player_up:
             self.scoreboard[self.player_up].add_marbles()
             self.marbles_in_hand = 0
             self.currently_moving = False
@@ -75,7 +78,7 @@ class Board():
                 print('Skip this store: {} marbles remain in hand'.format(self.marbles_in_hand))
                 self.switch_sides()
         else:
-            print('Skip this store: {} marbles remain in hand'.format(n_marbles))
+            self.add_marble_to_hole()
 
     def add_marble_to_hole(self):
         self.game_board[self.side_i][self.hole_i].add_marbles()
@@ -139,12 +142,14 @@ class Board():
         outer_board = None
         player_i = None
         hole_i = None
+        opposite_hole_i = None
         n_marbles = 4
 
         def __init__(self, board, side_i, hole_i, outer_board):
             self.outer_board = outer_board
             self.player_i = side_i
             self.hole_i = hole_i
+            self.opposite_hole_i = 5 - hole_i
             position_x = 0.15 + 0.12*hole_i if side_i==0 else 0.75 - 0.12*hole_i
             self.hole_label = tk.Label(board, text=self.n_marbles, bg='#c87e4f')
             self.hole_label.place(relheight=0.2,relwidth=0.1,relx=position_x,rely=0.7-side_i*.6)
@@ -164,24 +169,27 @@ class Board():
 
         def hole_clicked(self, other):
             if self.outer_board.player_up!=self.player_i:
-                print('Wrong side of the board for Player', str(self.outer_board.player_up+1))
+                print('Wrong side of the board for Player {}'.format(str(self.outer_board.player_up)))
             elif self.n_marbles==0:
-                print('Please select a hole on Player {}\'s side with marbles in it.'.format(str(self.outer_board.player_up+1)))
+                print('Please select a hole on Player {}\'s side with marbles in it.'.format(str(self.outer_board.player_up)))
             else:
-                self.get_marbles()
+                self.outer_board.side_i = self.player_i
+                self.outer_board.hole_i = self.hole_i + 1
+                self.outer_board.marbles_in_hand = self.get_marbles()
+                self.outer_board.currently_moving = True
+                self.outer_board.move_marbles()
+
+        def is_empty(self):
+            return self.n_marbles==0
 
         def get_marbles(self):
-            if self.n_marbles == 0:
-                print('From SIDE {} & HOLE {}: NO marbles grabbed!!'.format(self.player_i,self.hole_i))
-                self.n_marbles = 1
-                self.update_hole_label()
-                return False
-            else:
-                print('From SIDE {} & HOLE {}: {} marbles grabbed'.format(self.player_i,self.hole_i,self.n_marbles))
-                temp = self.n_marbles
-                self.n_marbles = 0
-                self.update_hole_label()
-                self.outer_board.move_marbles(self.player_i,self.hole_i,temp)
+            print('From SIDE {} & HOLE {}: {} marbles grabbed'.format(self.player_i,self.hole_i,self.n_marbles))
+            temp = self.n_marbles
+            self.n_marbles = 0
+            self.update_hole_label()
+            return temp
+            
+        def get_opposite_hole(self):
+            return self.outer_board.game_board[flip(self.player_i)][self.opposite_hole_i]
 
 flip = lambda x: int(not x)
-            
